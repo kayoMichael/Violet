0d2ee350eba5c7c17de9620d7bd9983155f89de8
kayoMichael <michaelxkayo@hotmail.com>
2024-02-05 18:24:04 +0900

    Create Shema for Ticket

---
+++ prisma/migrations/20240205091611_create_ticket_model/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Ticket` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(300) NOT NULL,
+    `description` TEXT NOT NULL,
+    `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN',
+    `date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedTIme` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
