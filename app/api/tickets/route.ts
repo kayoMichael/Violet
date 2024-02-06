b74e18627e0603f6ec8ddbd8448948b0890544ab
kayoMichael <michaelxkayo@hotmail.com>
2024-02-06 19:26:33 +0900

    Set Client side Validatio using zod

--- app/api/tickets/route.ts
+++ app/api/tickets/route.ts
@@ -1,12 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { z } from 'zod'
 import prisma from '@/prisma/client';
-
-const ticketSchema = z.object({
-    title: z.string().min(1, { message: "The title must be at least 1 character !"}).max(300, { message: "The title must be at least 300 character!"}),
-    description: z.string().min(1, { message: "The submission must be at least 1 character !"})
-})
-
+import { ticketSchema } from '../../../components/validations/ticketSchema';
 export async function POST(request: NextRequest) {
     const body = await request.json();
 
