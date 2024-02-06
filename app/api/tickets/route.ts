5472805148f0705845c997530a863508dde2d3f5
kayoMichael <michaelxkayo@hotmail.com>
2024-02-06 19:38:45 +0900

    configure ticket schema route

--- app/api/tickets/route.ts
+++ app/api/tickets/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/prisma/client';
-import { ticketSchema } from '../../../components/validations/ticketSchema';
+import { ticketSchema } from '../../../components/validations/schema';
 export async function POST(request: NextRequest) {
     const body = await request.json();
 
