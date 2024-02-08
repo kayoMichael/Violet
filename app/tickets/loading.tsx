f8cb5d67a582299f053f38209e41252821ef0a69
kayoMichael <michaelxkayo@hotmail.com>
2024-02-08 21:01:05 +0900

    Create Skeleton and Ticket Table

---
+++ app/tickets/loading.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import TicketButton from "./ticketButton";
+
+const LoadingTicketsPage = () => {
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return (
+    <>
+      <TicketButton />
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Ticket</th>
+                <th>Status</th>
+                <th className="hidden md:table-cell">Created</th>
+              </tr>
+            </thead>
+            <tbody>
+              {skeletons.map((ticket) => (
+                <tr key={ticket}>
+                  <td>
+                    <Skeleton />
+                  </td>
+                  <td>
+                    <Skeleton />
+                  </td>
+                  <td className="hidden md:table-cell">
+                    <Skeleton />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoadingTicketsPage;
