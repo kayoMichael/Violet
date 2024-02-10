b1bd7ac169fb3a4b6cc658baa6f7aa0d04a2fad5
kayoMichael <michaelxkayo@hotmail.com>
2024-02-10 16:10:59 +0900

    Set up updating tickets

--- app/tickets/[id]/edit/page.tsx
+++ app/tickets/[id]/edit/page.tsx
@@ -1,5 +1,21 @@
-const EditTicketPage = () => {
-  return <div>EditTicketPage</div>;
+import TicketForm from "@/components/form/ticketForm";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: { id: string };
+}
+const EditTicketPage = async ({ params }: Props) => {
+  const ticket = await prisma.ticket.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!ticket) {
+    notFound();
+  }
+  return <TicketForm ticket={ticket}></TicketForm>;
 };
 
 export default EditTicketPage;
