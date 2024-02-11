cca8a476492b3ca3578b84e10347b2858fd035e2
kayoMichael <michaelxkayo@hotmail.com>
2024-02-11 10:09:42 +0900

    fix error

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar /> {children}
+        <Toaster />
       </body>
-      <Toaster />
     </html>
   );
 }
