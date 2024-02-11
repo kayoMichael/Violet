4f85ad70e6414e14f30108d964b9fb89dca54995
kayoMichael <michaelxkayo@hotmail.com>
2024-02-11 18:07:19 +0900

    Add relation to user and ticket

--- app/tickets/[id]/assign.tsx
+++ app/tickets/[id]/assign.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { User } from "@prisma/client";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import { useEffect, useState } from "react";
 import {
   Select,
@@ -8,28 +10,31 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useQuery } from "@tanstack/react-query";
 const AssignTicket = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  useEffect(() => {
-    const fetchUsers = async () => {
-      await fetch("/api/users", {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () =>
+      fetch("/api/users", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
-      })
-        .then((res) => res.json())
-        .then((data) => setUsers(data));
-    };
-    fetchUsers();
-  }, []);
+      }).then((res) => res.json()),
+    staleTime: 60 * 1000,
+  });
+  if (isLoading) return <Skeleton width="6rem" height="3rem" circle={true} />;
   return (
     <Select>
       <SelectTrigger className="w-auto h-12">
         <SelectValue placeholder="Assign..." />
       </SelectTrigger>
       <SelectContent>
-        {users.map((user) => (
+        {users?.map((user) => (
           <SelectItem key={user.id} value={user.id}>
             {user.name}
           </SelectItem>
