147e1887f7dfe70607e9d94f609ef0101492b4f2
kayoMichael <michaelxkayo@hotmail.com>
2024-02-11 16:29:34 +0900

    import prisma

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/prisma/client';
 export async function GET(NextRequest: NextRequest) {
     const users = await prisma?.user.findMany({
         orderBy: {
