0747fd473324757a44290bd00ea79521381fce18
kayoMichael <michaelxkayo@hotmail.com>
2024-02-13 15:53:00 +0900

    update login page

---
+++ app/tickets/layout.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from "react";
+
+import Navbar from "@/components/general/Navbar";
+import { Toaster } from "@/components/ui/toaster";
+const mainPageLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div>
+      {" "}
+      <Navbar /> {children}
+      <Toaster />
+    </div>
+  );
+};
+
+export default mainPageLayout;
