78ca30d0862738f0afff07f491a32f3a1d50ea28
kayoMichael <michaelxkayo@hotmail.com>
2024-05-31 14:30:25 -0400

    refactor actions

--- lib/env.ts
+++ lib/env.ts
@@ -4,7 +4,6 @@ const envSchema = z.object({
   NODE_ENV: z.string().default('development'),
   DATABASE_URL: z.string(),
   DIRECT_URL: z.string(),
-  NEXTAUTH_URL: z.string().default('http://localhost:3000'),
   NEXTAUTH_SECRET: z.string().min(16),
 });
 
