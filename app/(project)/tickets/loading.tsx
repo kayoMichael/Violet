2fcc4127ddf895120b16c9ad90884ea716ec3d44
kayoMichael <michaelxkayo@hotmail.com>
2024-06-10 10:20:16 -0400

    refactor: skeleton style and syntax error

--- app/(project)/tickets/loading.tsx
+++ app/(project)/tickets/loading.tsx
@@ -9,31 +9,46 @@ const LoadingTicketsPage = () => {
   return (
     <>
       <div>
-        <div className='overflow-x-auto'>
-          <table className='table'>
-            <thead>
-              <tr>
-                <th>Ticket</th>
-                <th>Status</th>
-                <th className='hidden md:table-cell'>Created</th>
-              </tr>
-            </thead>
-            <tbody>
-              {skeletons.map((ticket) => (
-                <tr key={ticket}>
-                  <td>
-                    <Skeleton />
-                  </td>
-                  <td>
-                    <Skeleton />
-                  </td>
-                  <td className='hidden md:table-cell'>
-                    <Skeleton />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+        <div className='hidden h-full flex-1 flex-col space-y-8 p-8 md:flex'>
+          <div className='flex items-center justify-between space-y-2'>
+            <div>
+              <h2 className='text-2xl font-bold tracking-tight'>
+                <Skeleton width={10} />
+              </h2>
+              <p className='text-muted-foreground'>
+                Here&apos;s a list of your Registered Tickets!
+              </p>
+            </div>
+            <div className='flex items-center space-x-2'></div>
+          </div>
+          <div>
+            <div className='overflow-x-auto'>
+              <table className='table'>
+                <thead>
+                  <tr>
+                    <th>Ticket</th>
+                    <th>Status</th>
+                    <th className='hidden md:table-cell'>Created</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {skeletons.map((ticket) => (
+                    <tr key={ticket}>
+                      <td>
+                        <Skeleton />
+                      </td>
+                      <td>
+                        <Skeleton />
+                      </td>
+                      <td className='hidden md:table-cell'>
+                        <Skeleton />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,13 +1,23 @@
 import { NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+
+import authOptions from '../../auth/authOptions';
 
 import type { NextRequest } from 'next/server';
+
+import prisma from '@/prisma/client';
 
 export async function PATCH(
   request: NextRequest,
   { params: { id } }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
   const body = await request.json();
-  const user = await prisma?.user.update({
+  const user = await prisma.user.update({
     where: { id },
     data: {
       name: body.name || undefined,
